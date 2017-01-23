4bfedca3b0dce85823ec3d93f369c720fbdd0bb5
Ken Hasselmann ken@kenh.fr <khasselmann@submit-1-0.local>
2017-01-23 17:17:08 +0100

    del expe

---
+++ loop-functions/GiandujaStopLoopFunc.h
@@ -0,0 +1,43 @@
+#ifndef GIANDUJA_STOP_LOOP_FUNC
+#define GIANDUJA_STOP_LOOP_FUNC
+
+#include "../src/core/AutoMoDeLoopFunctions.h"
+#include <argos3/core/simulator/space/space.h>
+#include <argos3/plugins/robots/e-puck/simulator/epuck_entity.h>
+
+using namespace argos;
+
+class GiandujaStopLoopFunction: public AutoMoDeLoopFunctions {
+  public:
+    GiandujaStopLoopFunction();
+    GiandujaStopLoopFunction(const GiandujaStopLoopFunction& orig);
+    virtual ~GiandujaStopLoopFunction();
+
+    virtual void Destroy();
+
+    virtual argos::CColor GetFloorColor(const argos::CVector2& c_position_on_plane);
+    virtual void PostExperiment();
+    virtual void PostStep();
+    virtual void Init(TConfigurationNode& t_tree);
+    void PositionRobots();
+
+    Real GetObjectiveFunction();
+  private:
+    Real m_fRadius;
+    CVector2 m_cCoordSpot1;
+    //CVector2 m_CCoordRect1;
+    //CVector2 m_CCoordRect2;
+    CVector2 m_CCoordRect2Pos;
+    CVector2 m_CCoordRect1Pos;
+
+    typedef std::map<CEPuckEntity*, CVector2 > TOldPosMap;
+    TOldPosMap m_tOldPosPoints;
+
+    UInt8 m_unState;
+    UInt32 m_unCostI;
+    UInt32 m_unCostO;
+    UInt32 m_unTbar;
+    Real m_fObjectiveFunction;
+};
+
+#endif
