d9d27ab67ee989a604b8be300f4f1de2c1468826
Fahima <df_mokhtari@esi.dz>
2022-12-26 13:05:16 -0300

    fix:examples/scenario.txt: adding . to dependencyFile

---
+++ build/src/CMakeFiles/automode.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "visualize_fsm_automoc.cpp"
+  "automode_main_automoc.cpp"
+  "automode_automoc.cpp"
+  "CMakeFiles/automode.dir/core/AutoMoDeController.cpp.o"
+  "CMakeFiles/automode.dir/core/AutoMoDeFiniteStateMachine.cpp.o"
+  "CMakeFiles/automode.dir/core/AutoMoDeFsmBuilder.cpp.o"
+  "CMakeFiles/automode.dir/core/AutoMoDeFsmHistory.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviour.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourAntiPhototaxis.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourPhototaxis.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourAttraction.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourRepulsion.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourStop.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeBehaviourExploration.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeCondition.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionBlackFloor.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionWhiteFloor.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionGrayFloor.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionNeighborsCount.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionInvertedNeighborsCount.cpp.o"
+  "CMakeFiles/automode.dir/modules/AutoMoDeConditionFixedProbability.cpp.o"
+  "CMakeFiles/automode.dir/automode_automoc.cpp.o"
+  "libautomode.pdb"
+  "libautomode.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/automode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
